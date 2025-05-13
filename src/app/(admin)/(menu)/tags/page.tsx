c29e5120b65d3d6d479d53bea842781b2b0b7d6e
ezhilselvan1109 <ezhilselvan1109@gmail.com>
2025-05-13 18:06:49 +0530

    again

--- src/app/(admin)/(menu)/tags/page.tsx
+++ src/app/(admin)/(menu)/tags/page.tsx
@@ -1,12 +1,15 @@
+import { Suspense } from "react";
 import TagsClient from "./TagsClient";
 import ComponentCard from "@/components/common/ComponentCard";
 
 export default function TagsPage() {
   return (
       <div className="space-y-6">
-        <ComponentCard title="Basic Table 1">
+      <ComponentCard title="Basic Table 1">
+        <Suspense fallback={<div>Loading...</div>}>
           <TagsClient />
-        </ComponentCard>
-      </div>
+        </Suspense>
+      </ComponentCard>
+    </div>
   );
 }
