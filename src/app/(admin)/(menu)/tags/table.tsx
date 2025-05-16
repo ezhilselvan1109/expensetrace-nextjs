7cdf89154481a9ad24ad4ea3fd1057e1d30d83ca
ezhilselvan1109 <ezhilselvan1109@gmail.com>
2025-05-17 01:10:11 +0530

    accounts changes

--- src/app/(admin)/(menu)/tags/table.tsx
+++ src/app/(admin)/(menu)/tags/table.tsx
@@ -46,18 +46,17 @@ export default function TableContent({ onEdit, reloadFlag }: Props) {
   return (
     <div className="overflow-hidden rounded-xl border border-gray-200 bg-white dark:border-white/[0.05] dark:bg-white/[0.03]">
       <div className="max-w-full overflow-x-auto">
-        <div className="min-w-[1102px]">
           <Table>
             <TableHeader className="border-b border-gray-100 dark:border-white/[0.05]">
               <TableRow>
                 <TableCell
                   isHeader
-                  className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400">
+                  className="px-5 py-3 font-medium text-gray-500 text-center text-theme-xs dark:text-gray-400">
                   Name
                 </TableCell>
                 <TableCell
                   isHeader
-                  className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400">
+                  className="px-5 py-3 font-medium text-gray-500 text-center text-theme-xs dark:text-gray-400">
                   Actions
                 </TableCell>
               </TableRow>
@@ -66,10 +65,11 @@ export default function TableContent({ onEdit, reloadFlag }: Props) {
               {tags.map((tag) => (
                 <TableRow key={tag.id}>
                   <TableCell
-                    className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
+                    className="px-4 py-3 text-gray-500 text-center text-theme-sm dark:text-gray-400">
                     {tag.name}
                   </TableCell>
-                  <TableCell>
+                  <TableCell
+                    className="px-4 py-3 text-gray-500 text-center text-theme-sm dark:text-gray-400">
                     <button
                       onClick={() => onEdit(tag)}
                       className="text-blue-500 hover:underline mr-4"
@@ -87,7 +87,6 @@ export default function TableContent({ onEdit, reloadFlag }: Props) {
               ))}
             </TableBody>
           </Table>
-        </div>
       </div>
     </div>
   );
